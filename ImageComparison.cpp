44d325b64da3eb616eb3869f9a8233ce091c78f7
Erik Nilson <eriknielsen@users.noreply.github.com>
2025-04-11 13:07:50 +0200

    Initial version of the program that can compare 2 images and return a float value. Parsing PNG with help of lodepng library

--- ImageComparison.cpp
+++ ImageComparison.cpp
@@ -1,20 +1,73 @@
 // ImageComparison.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
-#include <iostream>
+#include "lodepng.h"
 
-int main()
-{
-    std::cout << "Hello World!\n";
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <algorithm>
+#include <iomanip>
+
+/// <summary>
+/// Compares 2 vectors and assumes each pixel is 4 bytes large ordered by RGBA
+/// Does not compare alpha
+/// </summary>
+/// <param name="image"></param>
+/// <param name="image2"></param>
+/// <returns>Value from 0-1 where 1 is most similar and 0 is not similar</returns>
+float compare(std::vector<unsigned char> &image, std::vector<unsigned char>& image2) {
+
+    const float color_count = 255;
+    float similarity_sum = 0;
+    for (size_t i = 0; i < image.size(); i += 4)
+    {
+        float similarity = 0;
+        similarity += 1 - std::abs(image[i] - image2[i]) / color_count;
+        similarity += 1 - std::abs(image[i + 1] - image2[i + 1]) / color_count;
+        similarity += 1 - std::abs(image[i + 2] - image2[i + 2]) / color_count;
+        similarity /= 3;
+
+        similarity_sum += similarity;
+    }
+
+    similarity_sum = std::roundf(similarity_sum / (image.size() / 4) * 1000) / 1000;
+
+    return similarity_sum;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
+bool tryDecodeOneStep(const char* filename, std::vector<unsigned char>& image, unsigned int& width, unsigned int& height) {
+    unsigned error;
 
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+    error = lodepng::decode(image, width, height, filename);
+    if (error) {
+        printf("error %u: %s\n", error, lodepng_error_text(error));
+        return false;
+    }
+
+    return true;
+}
+
+int main(int argc, char* argv[])
+{
+    const char* filename = argc > 1 ? argv[1] : "image1.png";
+    std::vector<unsigned char> image1;
+    unsigned int width, height;
+    if (!tryDecodeOneStep(filename, image1, width, height)) {
+        std::cout << "Decoding image1 failed";
+        return 1;
+    }
+    const char* filename2 = argc > 1 ? argv[1] : "image2.png";
+    std::vector<unsigned char> image2;
+    unsigned int width2, height2;
+    if (!tryDecodeOneStep(filename2, image2, width2, height2)) {
+        std::cout << "Decoding image2 failed";
+        return 1;
+    }
+
+    
+    float similarity_factor = compare(image1, image2);
+
+    std::cout << "Images are a factor of " << similarity_factor << " similar";
+}
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # ImageComparison
+## TODO
+- [x] Compare 2 textures and give a value between 0-1 on how similar they are
+- [x] Import the texture data from PNG
+- [] Experiment with nearest neighbour to deal with difference in resolutions
+- [] Produce a delta image
+- [] Save the result, the delta image and the compared images in a folder
