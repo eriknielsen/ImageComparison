91aff35d48cbda446dce98d32a881919be5685a0
Erik Nilson <eriknielsen@users.noreply.github.com>
2025-04-14 09:52:25 +0200

    Made images smaller

--- Aggregator.ps1
+++ Aggregator.ps1
@@ -6,6 +6,8 @@ param(
 
 # Get absolute path to comparer and output folder
 $pathToImageComparer = Join-Path (Split-Path -Parent $MyInvocation.MyCommand.Path) "ImageComparison.exe"
+# Make sure output folder exists
+New-Item -ItemType Directory -Path $OutputFolder -Force | Out-Null
 $pathToOutput = Resolve-Path $OutputFolder
 
 # Clear out the output folder
@@ -14,12 +16,10 @@ Remove-Item -Recurse -Force $OutputFolder
 # For each folder in the input, assume there's 2 images to compare 
 $inputFolders = Get-ChildItem -Path $InputFolder -Directory
 foreach ($folder in $inputFolders) {
-    Write-Host "Processing folder: $($folder.FullName)"
-
     Push-Location $folder.FullName
     try {
         $similarity = & $pathToImageComparer a.png b.png "results"
-
+        Write-Host "Processing set: $($folder.FullName). Similarity factor of $similarity"
         # If input is too different (graphical artifacts have been introduced)
         # copy the input to the output for further processing
         if ($similarity -lt $MinimumSimilarity) {
